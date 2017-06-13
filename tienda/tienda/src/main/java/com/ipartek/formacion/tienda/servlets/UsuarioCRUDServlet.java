5ee6cf93d0f368c5a57af3fb6d77d540d364d103
jonantu1989 <jonantunano@gmail.com>
2017-06-13 19:12:28 +0200

    cambios en rutas

--- tienda/tienda/src/main/java/com/ipartek/formacion/tienda/servlets/UsuarioCRUDServlet.java
+++ tienda/tienda/src/main/java/com/ipartek/formacion/tienda/servlets/UsuarioCRUDServlet.java
@@ -15,7 +15,7 @@ import com.ipartek.formacion.tienda.tipos.Usuario;
 public class UsuarioCRUDServlet extends HttpServlet {
 	static final String RUTA_FORMULARIO = "/WEB-INF/vistas/usuarioform.jsp";
 	static final String RUTA_LISTADO = "/WEB-INF/vistas/usuariocrud.jsp";
-	static final String RUTA_SERVLET_LISTADO = "/usuario";
+	static final String RUTA_SERVLET_LISTADO = "/usuariocrud";
 
 	private static final long serialVersionUID = 1L;
 
