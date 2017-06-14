a1ca78e8d41b9b16aadefffe23b3cbeb4b7206a1
jonantu1989 <jonantunano@gmail.com>
2017-06-14 21:24:06 +0200

    Clases (producto y usuario) hechas

---
+++ Carrito/tiposC/src/main/java/com/ipartek/formacion/carrito/tipos/Producto.java
@@ -0,0 +1,100 @@
+package com.ipartek.formacion.carrito.tipos;
+
+public class Producto {
+	private int id;
+	private String nombre;
+	private String descripcion;
+	private String precio;
+	
+	public Producto(int id, String nombre, String descripcion, String precio) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.precio = precio;
+	}
+
+	public Producto() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(String precio) {
+		this.precio = precio;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((descripcion == null) ? 0 : descripcion.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		result = prime * result + ((precio == null) ? 0 : precio.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Producto other = (Producto) obj;
+		if (descripcion == null) {
+			if (other.descripcion != null)
+				return false;
+		} else if (!descripcion.equals(other.descripcion))
+			return false;
+		if (id != other.id)
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		if (precio == null) {
+			if (other.precio != null)
+				return false;
+		} else if (!precio.equals(other.precio))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Producto [id=" + id + ", nombre=" + nombre + ", descripcion="
+				+ descripcion + ", precio=" + precio + "]";
+	}
+	
+}
---
+++ Carrito/tiposC/src/main/java/com/ipartek/formacion/carrito/tipos/Usuario.java
@@ -0,0 +1,113 @@
+package com.ipartek.formacion.carrito.tipos;
+
+public class Usuario {
+	private int id;
+	private int id_roles;
+	private String nombre;
+	private String pass;
+	private String username;
+	public Usuario(int id, int id_roles, String nombre, String pass,
+			String username) {
+		super();
+		this.id = id;
+		this.id_roles = id_roles;
+		this.nombre = nombre;
+		this.pass = pass;
+		this.username = username;
+	}
+	
+	public Usuario() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getId_roles() {
+		return id_roles;
+	}
+
+	public void setId_roles(int id_roles) {
+		this.id_roles = id_roles;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + id_roles;
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		result = prime * result + ((pass == null) ? 0 : pass.hashCode());
+		result = prime * result
+				+ ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (id != other.id)
+			return false;
+		if (id_roles != other.id_roles)
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		if (pass == null) {
+			if (other.pass != null)
+				return false;
+		} else if (!pass.equals(other.pass))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", id_roles=" + id_roles + ", nombre="
+				+ nombre + ", pass=" + pass + ", username=" + username + "]";
+	}
+	
+}
