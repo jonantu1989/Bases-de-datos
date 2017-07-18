46e3a885ef5c21ac2e7c724e088d421de8d56242
jonantu1989 <jonantunano@gmail.com>
2017-07-18 15:52:16 +0200

    listener de inicializacion,  gitignores y wel web.xml por el listener

---
+++ Carrito/webC/src/main/java/com/ipartek/formacion/listeners/Inicializacion.java
@@ -0,0 +1,20 @@
+package com.ipartek.formacion.listeners;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class Inicializacion implements ServletContextListener {
+
+	public Inicializacion() {
+
+	}
+
+	public void contextDestroyed(ServletContextEvent sce) {
+
+	}
+
+	public void contextInitialized(ServletContextEvent sce) {
+
+	}
+
+}
