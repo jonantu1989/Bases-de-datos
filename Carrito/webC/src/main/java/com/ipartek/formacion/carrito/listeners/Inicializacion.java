b48abb0a5edddfd782eca76ac0d467b15b80343b
jonantu1989 <jonantunano@gmail.com>
2017-07-19 16:18:38 +0200

    listener de inicializacion en marcha

--- Carrito/webC/src/main/java/com/ipartek/formacion/carrito/listeners/Inicializacion.java
+++ Carrito/webC/src/main/java/com/ipartek/formacion/carrito/listeners/Inicializacion.java
@@ -1,21 +1,32 @@
 package com.ipartek.formacion.carrito.listeners;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
+import org.apache.log4j.Logger;
+
 @WebListener
 public class Inicializacion implements ServletContextListener {
+
+	private static Logger log = Logger.getLogger(Inicializacion.class);
 
 	public Inicializacion() {
 
 	}
 
-	public void contextDestroyed(ServletContextEvent sce) {
+	public void contextDestroyed(ServletContextEvent agr0) {
 
 	}
 
-	public void contextInitialized(ServletContextEvent sce) {
+	public void contextInitialized(ServletContextEvent servletContextEvent) {
+
+		ServletContext application = servletContextEvent.getServletContext();
+
+		String path = application.getContextPath();
+		application.setAttribute("Ruta", path);
+		log.info("Almacenada la ruta relativa de la aplicación: " + path);
 
 	}
 
