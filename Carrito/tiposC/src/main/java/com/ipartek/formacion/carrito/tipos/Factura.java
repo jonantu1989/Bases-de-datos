db800cf220b74a69b7d829567550b1dbd5d24b15
jonantu1989 <jonantunano@gmail.com>
2017-07-26 19:50:32 +0200

    clase Factura creada y clase Factura modificada

--- Carrito/tiposC/src/main/java/com/ipartek/formacion/carrito/tipos/Factura.java
+++ Carrito/tiposC/src/main/java/com/ipartek/formacion/carrito/tipos/Factura.java
@@ -1,6 +1,8 @@
 package com.ipartek.formacion.carrito.tipos;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Factura {
 
@@ -8,6 +10,8 @@ public class Factura {
 	private String número_factura;
 	private int id_usuarios;
 	private Date fecha;
+
+	private List<FacturaLinea> lineas = new ArrayList<FacturaLinea>();
 
 	public Factura(int id, String número_factura, int id_usuarios, Date fecha) {
 		super();
@@ -25,38 +29,85 @@ public class Factura {
 		return id;
 	}
 
-	public String getNúmero_factura() {
-		return número_factura;
-	}
-
-	public int getId_usuarios() {
-		return id_usuarios;
-	}
-
-	public Date getFecha() {
-		return fecha;
-	}
-
 	public void setId(int id) {
 		this.id = id;
+	}
+
+	public String getNúmero_factura() {
+		return número_factura;
 	}
 
 	public void setNúmero_factura(String número_factura) {
 		this.número_factura = número_factura;
 	}
 
+	public int getId_usuarios() {
+		return id_usuarios;
+	}
+
 	public void setId_usuarios(int id_usuarios) {
 		this.id_usuarios = id_usuarios;
+	}
+
+	public Date getFecha() {
+		return fecha;
 	}
 
 	public void setFecha(Date fecha) {
 		this.fecha = fecha;
 	}
 
+	public void addProductoYCantidad(Producto producto, int cantidad) {
+		lineas.add(new FacturaLinea(producto, cantidad));
+	}
+
+	public FacturaLinea[] getLineas() {
+		return lineas.toArray(new FacturaLinea[lineas.size()]);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((fecha == null) ? 0 : fecha.hashCode());
+		result = prime * result + id;
+		result = prime * result + id_usuarios;
+		result = prime * result
+				+ ((número_factura == null) ? 0 : número_factura.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Factura other = (Factura) obj;
+		if (fecha == null) {
+			if (other.fecha != null)
+				return false;
+		} else if (!fecha.equals(other.fecha))
+			return false;
+		if (id != other.id)
+			return false;
+		if (id_usuarios != other.id_usuarios)
+			return false;
+		if (número_factura == null) {
+			if (other.número_factura != null)
+				return false;
+		} else if (!número_factura.equals(other.número_factura))
+			return false;
+		return true;
+	}
+
 	@Override
 	public String toString() {
 		return "Factura [id=" + id + ", número_factura=" + número_factura
-				+ ", id_usuarios=" + id_usuarios + ", fecha=" + fecha + "]";
+				+ ", id_usuarios=" + id_usuarios + ", fecha=" + fecha + "\n"
+				+ lineas.toString() + "]";
 	}
 
 }
---
+++ Carrito/tiposC/src/main/java/com/ipartek/formacion/carrito/tipos/FacturaLinea.java
@@ -0,0 +1,65 @@
+package com.ipartek.formacion.carrito.tipos;
+
+public class FacturaLinea {
+
+	private Producto producto;
+	private int cantidad;
+
+	public FacturaLinea(Producto producto, int cantidad) {
+		super();
+		this.producto = producto;
+		this.cantidad = cantidad;
+	}
+
+	public Producto getProducto() {
+		return producto;
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + cantidad;
+		result = prime * result
+				+ ((producto == null) ? 0 : producto.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FacturaLinea other = (FacturaLinea) obj;
+		if (cantidad != other.cantidad)
+			return false;
+		if (producto == null) {
+			if (other.producto != null)
+				return false;
+		} else if (!producto.equals(other.producto))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "FacturaLinea [producto=" + producto + ", cantidad=" + cantidad
+				+ "]";
+	}
+
+}
